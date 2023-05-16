22b64533a0d00ef38bbecbd2e5f473576e099233
nmare <nmare@DESKTOP-9VL7QGI>
2023-05-16 20:42:10 +0300

    Add project files.

---
+++ Numeroloogia Nime arv/Numeroloogia_Nime_arv.py
@@ -0,0 +1,77 @@
+﻿from tkinter import *
+from tkinter.messagebox import *
+from functions import *
+
+"""
+Чтение текстовых файлов: 
+letters.txt, numbers.txt, one.txt, ... nine.txt, 
+в которых содержится информация, используемая в расчетах числа имени.
+"""
+letters = readFromFile("letters.txt")
+numbers = readFromFile("numbers.txt")
+one = readFromFile("one.txt")
+two = readFromFile("two.txt")                                         
+three = readFromFile("three.txt")
+four = readFromFile("four.txt")
+five = readFromFile("five.txt")
+six = readFromFile("six.txt")
+seven = readFromFile("seven.txt")
+eight = readFromFile("eight.txt")
+nine = readFromFile("nine.txt")
+
+"""
+Далее создаем главное окно программы, 
+устанавливаем его свойства и задается фон.
+"""
+window = Tk()
+window.title("Нумерология")
+window.geometry("1700x1000+10+105") #(ширина окна, высота окна, x - на сколько в право открывать окно, y - на сколько вниз открывать окно )
+window.resizable(width=False, height=False) #нельзя изменять размер окна
+window.configure(bg="thistle") 
+
+
+# Затем создаем виджеты (элементы интерфейса):
+
+L0 = Label (window, height= 2, width = 10, bg = "thistle") # пустой лейбл для добавления отступа
+l1 = Label (window, width = 40, text ="Хочешь узнать число своего имени? \nВведи имя: ", font = "Arial 20", fg = "black", bg="thistle") # лейбл с текстом
+e = Entry (window, font = "Arial 30", fg = "black", bg = "khaki") # поле ввода для имени
+b = Button (window, height= 2, width = 10, text ="Узнать!", font = "Arial 18", fg = "black", bg = "khaki", command = lambda:nameNamber(letters,numbers,e.get().lower(),l3)) 
+# кнопка, при нажатии на которую происходит вызов функции nameNamber с передачей аргументов letters, numbers, e.get().lower(), l3;
+l2 = Label (window, width = 30, text ="Число твоего имени: ", font = "Coral 20", fg = "black", bg="thistle")
+l3 = Label (window, font = "Coral 12", bg = "thistle", fg = "black") # лейбл, куда будет выводиться результат расчета числа имени
+l5 = Label (window, height= 1, width = 20, bg = "thistle")
+l6 = Label (window, height= 1, width = 20, bg = "thistle")
+l8 = Label (window, height= 1, width = 20, bg = "thistle")
+l9 = Label (window, height= 1, width = 20, bg = "thistle")
+l10 = Label (window, height= 1, width = 20, bg = "thistle")
+l11 = Label (window, height= 1, width = 20, bg = "thistle")
+l12 = Label (window, height= 1, width = 20, bg = "thistle")
+l13 = Label (window, height= 1, width = 20, bg = "thistle")
+
+"""
+Далее происходят операции по позиционированию созданных элементов на окне 
+с помощью метода grid, который позволяет задать расположение элементов в 
+таблице с заданным числом строк и столбцов.
+"""
+L0.grid(row = 1, column = 1)
+l1.grid(row = 2, column = 1)
+e.grid(row = 4, column = 1)
+b.grid(row = 6, column = 1)
+l2.grid(row = 2, column = 2)
+l3.grid(row = 7, column = 2)
+l5.grid(row = 3, column = 1 )
+l6.grid(row = 5, column = 1 )
+l8.grid(row = 7, column = 1 )
+l9.grid(row = 1, column = 2 )
+l10.grid(row = 3, column = 2 )
+l11.grid(row = 4, column = 2 )
+l12.grid(row = 5, column = 2 )
+l13.grid(row = 6, column = 2 )
+
+"""
+И, наконец, запускается бесконечный цикл обработки событий окна с помощью метода mainloop. 
+Этот метод перехватывает все события, происходящие в окне (например, нажатия на кнопки) и 
+передает их для обработки.
+"""
+
+window.mainloop()
---
+++ Numeroloogia Nime arv/functions.py
@@ -0,0 +1,79 @@
+﻿
+# Функция readFromFile читает текст из файла и возвращает его в виде списка.
+
+def readFromFile (file:str)->list:
+    file = open (file, "r", encoding="UTF-8-sig")
+    mas = [] 
+    for line in file:
+        mas.append(line.strip())
+    file.close()
+    return mas  
+
+"""
+ Функция nameNamber принимает в качестве аргументов список букв и 
+ соответствующих им цифр, строку с именем и объект метки tkinter. 
+"""
+
+
+def nameNamber (l:list, n:list, name:str, lbl3):
+    name = list (name) 
+    numbersIndex = int() 
+
+    """
+    Вычисляем «число имени», складывая числа, связанные с каждой буквой в имени. 
+    Если это число меньше 10, оно возвращается как есть. 
+    Если оно больше или равно 10, цифры складываются, чтобы получить новое число, 
+    и это число возвращается.
+    """
+
+    for i in name:                  
+        lettersIndex = l.index(i)
+        numbersIndex += int (n[lettersIndex])
+    if numbersIndex <10:
+        text = f"Твоё число имени - {numbersIndex}"
+    elif numbersIndex >=10:
+        first = numbersIndex//10
+        second = numbersIndex%10
+        numbersIndex = first + second 
+        text = f"Твоё число имени - {numbersIndex}"
+    if numbersIndex >=10:
+        first = numbersIndex//10
+        second = numbersIndex%10
+        numbersIndex = first + second
+        text = f"Твоё число имени - {numbersIndex}"
+        """
+        В зависимости от значения «nameNamber» функция открывает файл с соответствующим именем 
+        (например, «one.txt» для числа 1) и считывает его содержимое. Затем содержимое 
+        отображается в метке tkinter, переданной в качестве аргумента.
+        """
+    if numbersIndex == 1:
+        file = open ("one.txt", 'r', encoding="UTF-8-sig")
+        f = file.read()
+    elif numbersIndex == 2:
+        file = open ("two.txt", 'r', encoding="UTF-8-sig")
+        f = file.read()
+    elif numbersIndex == 3:
+        file = open ("three.txt", 'r', encoding="UTF-8-sig")
+        f = file.read() 
+    elif numbersIndex == 4:
+        file = open ("four.txt", 'r', encoding="UTF-8-sig")
+        f = file.read()
+    elif numbersIndex == 5:
+        file = open ("five.txt", 'r', encoding="UTF-8-sig")
+        f = file.read()
+    elif numbersIndex == 6:
+        file = open ("six.txt", 'r', encoding="UTF-8-sig")
+        f = file.read()
+    elif numbersIndex == 7:
+        file = open ("seven.txt", 'r', encoding="UTF-8-sig")
+        f = file.read()
+    elif numbersIndex == 8:
+        file = open ("eight.txt", 'r', encoding="UTF-8-sig")
+        f = file.read()
+    elif numbersIndex == 9:
+        file = open ("nine.txt", 'r', encoding="UTF-8-sig")
+        f = file.read()
+    lbl3.configure(text = f)
+    return numbersIndex   #Наконец, функция возвращает «номер имени».
+
+    
